254f95ed81c3802bc890335454a4b26a9aaf61c7
Amit Patra <amitbaranpatra@gmail.com>
2023-03-12 10:56:22 +0530

    Data ingestion component

--- src/exception.py
+++ src/exception.py
@@ -9,7 +9,7 @@ def error_message_detail(error,error_detail:sys):
     return error_message
     
 
-class custom_exception(Exception):
+class customexception(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
@@ -18,9 +18,9 @@ class custom_exception(Exception):
         return self.error_message
     
 
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero error")
-        raise custom_exception(e,sys)
+# if __name__=="__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by zero error")
+#         raise customexception(e,sys)
