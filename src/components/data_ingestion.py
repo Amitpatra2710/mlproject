b9a89830d27edf81fb40239e31861007bb08ca2e
Amit Patra <amitbaranpatra@gmail.com>
2023-03-15 16:49:41 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -32,7 +32,7 @@ class dataIngestion:
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-            logging.info('Data ungestion completed')
+            logging.info('Data ingestion completed')
 
             return(
                 self.ingestion_config.train_data_path,
